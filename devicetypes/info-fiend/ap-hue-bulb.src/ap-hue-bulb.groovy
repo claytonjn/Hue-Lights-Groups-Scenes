ea1fc5fd56f74df13f29c67215d061e020b84364
claytonjn <claytonjn@users.noreply.github.com>
2016-03-16 21:44:40 -0400

    Reset to standard hue reset value
    This is how the bulb is set when automatically connected to power, and what I would expect the reset button to do.

--- devicetypes/info-fiend/ap-hue-bulb.src/ap-hue-bulb.groovy
+++ devicetypes/info-fiend/ap-hue-bulb.src/ap-hue-bulb.groovy
@@ -196,8 +196,7 @@ void setColor(value) {
 
 void reset() {
 	log.debug "Executing 'reset'"
-    def value = [level:100, hex:"#90C638", saturation:56, hue:23]
-    setAdjustedColor(value)
+    setColorTemperature(2710)
 	parent.poll()
 }
 
